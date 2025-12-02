3bbc825faa0930fc5078eaa1ba0a5f35d4ad1b01
AmsRahul <dapurmilky@gmail.com>
2025-12-02 22:31:46 +0800

    add env

--- services/sheet.py
+++ services/sheet.py
@@ -1,14 +1,27 @@
 import gspread
 from models.order import Order
 from google.oauth2.service_account import Credentials
+import os
+import json
+# from dotenv import load_dotenv
 
+# load_dotenv()
 # scopes API google sheets
 SCOPES = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive"
 ]
+json_creds = os.environ.get("GOOGLE_CREDENTIALS_JSON")
 
-creds = Credentials.from_service_account_file("service_account.json", scopes=SCOPES)
+if json_creds:
+    # 2. Parse string JSON menjadi dictionary Python
+    service_account_info = json.loads(json_creds)
+    
+    # 3. Gunakan method _info, bukan _file
+    creds = Credentials.from_service_account_info(service_account_info, scopes=SCOPES)
+else:
+    raise Exception("Credential Google tidak ditemukan di Environment Variables")
+
 client = gspread.authorize(creds)
 
 # masukkan ID spreadsheet kamu
